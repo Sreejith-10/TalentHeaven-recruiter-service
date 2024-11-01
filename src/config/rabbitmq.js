937ff5ce0f08070e47274f1fa8721145fdf9973f
sreejith <sreejithsree956266@gmail.com>
2024-11-01 18:04:01 +0530

    rabbitmq connection changes

--- src/config/rabbitmq.js
+++ src/config/rabbitmq.js
@@ -1,39 +1,46 @@
-import amqp from "amqplib/callback_api.js";
+import amqp from "amqplib";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-let queue = "job_service_queue";
+const queue = "recruiter_service_queue";
 let channel = null;
 
-export const createConnectionMQ = () => {
-	amqp.connect(process.env.AMQP_URL, async (err0, connection) => {
-		if (err0) {
-			throw err0;
-		}
-		channel = await connection.createChannel();
-		await channel.assertExchange(queue, "direct", {durable: false});
-		const q = await channel.assertQueue("", {exclusive: true});
-		console.log("Waiting for data");
-		channel.bindQueue(q.queue, queue, "JOB_POST");
-		channel.bindQueue(q.queue, queue, "OTP");
-		channel.consume(
-			q.queue,
-			(msg) => {
-				const key = msg.fields.routingKey;
+export const createConnectionMQ = async () => {
+  try {
+    const connection = await amqp.connect(process.env.AMQP_URL)
+    channel = await connection.createChannel();
+    console.log("Connected to AMQP Server")
+    await channel.assertQueue(queue, { durable: true });
+    console.log("Waiting for data");
+    channel.consume(
+      queue,
+      async (msg) => {
+        const { action, body } = JSON.parse(msg.content.toString())
+        const responseState = {}
 
-				switch (key) {
-					case "JOB_POST":
-						console.log(msg.content.toString());
-						break;
-					case "OPT":
-						console.log(msg.content.toString());
-						break;
-					default:
-						break;
-				}
-			},
-			{noAck: true}
-		);
-	});
+        switch (key) {
+          case "JOB_POST":
+            console.log(msg.content.toString());
+            break;
+          case "OPT":
+            console.log(msg.content.toString());
+            break;
+          default:
+            break;
+        }
+
+
+        await channel.sendToQueue(msg.properties.replyTo, Buffer.from(JSON.stringify(responseState)), {
+          correlationId: msg.properties.correlationId
+        })
+
+        channel.ack(msg)
+      },
+      { noAck: false }
+    );
+
+  } catch (error) {
+    throw new Error("RabbitMQ Error" + error)
+  }
 };
